326f3226dd108828139beb55d4ac8ff2f90058f5
anasty223 <anasty22@gmail.com>
2022-05-29 22:24:59 +0300

    .

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import your route components too
 
 render(
-  <BrowserRouter basename="/https://anasty223.github.io/goit-react-hw-05-movies_42/">
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
